19eeebc5b46f275d39eefb865a928ceb897835f5
Dan Parry <parryd424@gmail.com>
2017-04-24 13:17:30 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Dan"
+  end
+
+  get '/hometown' do
+    "My hometown is Whippany"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is DMX - Party Up"
+  end
+
+
 end
